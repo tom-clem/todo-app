33f61e7721069deea8539112c66241821480240e
ksmithut <ksmith.ut@gmail.com>
2015-10-13 10:27:49 -0600

    updates gulpfile with watchers and livereload

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
+var htmlmin = require('gulp-htmlmin');
+var express = require('express');
+var livereload = require('gulp-livereload');
 
 gulp.task('default', [
   'scripts',
@@ -12,32 +15,75 @@ gulp.task('default', [
   'templates'
 ]);
 
+gulp.task('watch', [
+  'scripts.watch',
+  'styles.watch',
+  'static.watch',
+  'templates.watch',
+  'server',
+  'livereload'
+]);
+
+
 gulp.task('scripts', function () {
   return gulp.src('src/scripts/**/*.js')
     .pipe(sourcemaps.init())
       .pipe(concat('app.min.js'))
+      .pipe(uglify())
     .pipe(sourcemaps.write('../maps'))
     .pipe(gulp.dest('build/js'));
 });
+gulp.task('scripts.watch', ['scripts'], function () {
+  gulp.watch('src/scripts/**/*.js', ['scripts']);
+});
+
 
 gulp.task('styles', function () {
-  console.log('ran styles');
+  return gulp.src('src/styles/**/*.css')
+    .pipe(sourcemaps.init())
+      .pipe(concat('app.min.css'))
+    .pipe(sourcemaps.write('../maps'))
+    .pipe(gulp.dest('build/css'));
+});
+gulp.task('styles.watch', ['styles'], function () {
+  gulp.watch('src/styles/**/*.css', ['styles']);
 });
 
+
 gulp.task('static', function () {
-  console.log('ran static');
+  return gulp.src('src/static/**/*')
+    .pipe(gulp.dest('build'));
 });
+gulp.task('static.watch', ['static'], function () {
+  gulp.watch('src/static/**/*', ['static']);
+});
+
 
 gulp.task('templates', function () {
   return gulp.src('src/templates/**/*.html')
+    .pipe(htmlmin({
+      collapseWhitespace: true
+    }))
     .pipe(gulp.dest('build'));
+});
+gulp.task('templates.watch', ['templates'], function () {
+  gulp.watch('src/templates/**/*.html', ['templates']);
 });
 
 
+gulp.task('server', function () {
+  var app = express();
+  app.use(express.static('build'));
+  app.listen(8000);
+});
 
 
-
-
+gulp.task('livereload', function () {
+  livereload.listen();
+  gulp.watch('build/**/*', function (event) {
+    livereload.changed(event);
+  });
+});
 
 
 
--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,5 @@
+console.log('hello world');
 console.log('foo bar');
+function hello(name) {
+  console.log(name);
+}

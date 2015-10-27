87be3073e03e0b95bfa50a9e0a72e547aa25e4c2
ksmithut <ksmith.ut@gmail.com>
2015-10-27 10:18:09 -0600

    adds login controller

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,1 +1,19 @@
+angular
+  .module('TodoApp', [
+    'ngRoute',
+    'LoginController',
+  ])
+  .config([
+    '$routeProvider',
+    function ($routeProvider) { 'use strict';
+      console.log($routeProvider);
+      $routeProvider
+        .when('/login', {
+          templateUrl: '/partials/login-controller.html',
+          controller: 'LoginController',
+        })
+        .otherwise('/login');
+    },
+  ]);
+
 

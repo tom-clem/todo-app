3274da5be0ea4f3c0d65cb8db217200161ba388f
ksmithut <ksmith.ut@gmail.com>
2015-11-19 09:37:21 -0700

    adds todos service

---
+++ src/scripts/services/dgm-todo/todos.js
@@ -0,0 +1,49 @@
+angular
+.module('dgmTodo.todos', [
+  'dgmTodo'
+])
+.factory('todos', [
+  '$http',
+  'dgmTodoHost',
+  function($http, host) {
+    return {
+      create: function(userId, data) {
+        return $http
+          .post(host + '/users/' + userId + '/todos', data)
+          .then(function(res) {
+            return res.data;
+          });
+      },
+      read: function(userId) {
+        return $http
+          .get(host + '/users/' + userId + '/todos')
+          .then(function(res) {
+            return res.data;
+          });
+      },
+      update: function(userId, id, data) {
+        return $http
+          .put(host + '/users/' + userId + '/todos/' + id, data)
+          .then(function(res) {
+            return res.data;
+          });
+      },
+      delete: function(userId, id) {
+        return $http
+          .delete(host + '/users/' + userId + '/todos/' + id)
+          .then(function(res) {
+            return res.data;
+          });
+      },
+    };
+  },
+]);
+
+
+
+
+
+
+
+
+
